9c06e7a48c691b3c0bd860f71d34ab46f77e7c7e
rachasalhi1 <162183624+rachasalhi1@users.noreply.github.com>
2025-04-17 21:28:27 +0200

    Update filtragex.m

--- filtragex.m
+++ filtragex.m
@@ -1,21 +1,43 @@
-% Spécifications
-fa = 200;           % Bande passante (Hz)
-fc = 350;           % Bande coupée (Hz)
+% ===== Question 3 - Application du filtre =====
+
+% Données du filtre
+fa = 200;           % fréquence de la bande passante
+fc = 350;           % fréquence de la bande atténuée
+wa = 2*pi*fa;       % pulsation de fa
+wc = 2*pi*fc;       % pulsation de fc
 ra = 2;             % Atténuation max en bande passante (dB)
 rc = 40;            % Atténuation min en bande coupée (dB)
 
-% Convertir en pulsations (rad/s)
-wa = 2*pi*fa;
-wc = 2*pi*fc;
+% Conception du filtre Butterworth analogique
+[n, wn] = buttord(wa, wc, ra, rc, 's'); % Calcul de l’ordre
+[b, a] = butter(n, wn, 's');            % Calcul des coefficients du filtre
 
-% Détermination de l'ordre et fréquence de coupure normalisée
-[n, wn] = buttord(wa, wc, ra, rc, 's');
+% Construction du signal x(t)
+fe = 2000;           % fréquence d’échantillonnage
+f1 = 100;            % fréquence dans la bande passante
+f2 = 400;            % fréquence hors bande
+t = 0:1/fe:1;        % vecteur temps de 1 seconde
 
-% Conception du filtre analogique
-[b, a] = butter(n, wn, 's');
+x1 = cos(2*pi*f1*t); % composante à 100 Hz
+x2 = cos(2*pi*f2*t); % composante à 400 Hz
+x = x1 + x2;         % signal composé
 
-% Transformer en filtre numérique via bilinear transform
-[bz, az] = bilinear(b, a, fe);   % fe = fréquence d’échantillonnage
+% Application du filtre analogique au signal
+sys = tf(b, a);        % création de la fonction de transfert
+y = lsim(sys, x, t);   % sortie filtrée du signal
 
-% Appliquer le filtre numérique au signal
-xf = filter(bz, az, x);
+% Affichage dans le domaine temporel
+figure;
+subplot(211);
+plot(t, x);
+grid on;
+axis([0 5/f1 -2 2]);
+xlabel('Temps (s)');
+title('Signal x(t) avant filtrage');
+
+subplot(212);
+plot(t, y);
+grid on;
+axis([0 5/f1 -2 2]);
+xlabel('Temps (s)');
+title('Signal y(t) après filtrage');
