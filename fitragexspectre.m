b3e79acb343e49593720f0f641b676a35af82da7
rachasalhi1 <162183624+rachasalhi1@users.noreply.github.com>
2025-04-17 21:35:14 +0200

    Create fitragexspectre.m

---
+++ fitragexspectre.m
@@ -0,0 +1,26 @@
+% ===== Question 4 - Spectres avant et après filtrage =====
+
+% Calcul du spectre du signal original x(t)
+spx = abs(fft(x)) / fe;   % spectre normalisé
+% Calcul du spectre du signal filtré y(t)
+spy = abs(fft(y)) / fe;
+
+% Axe des fréquences
+fr = linspace(0, fe, length(t));
+
+% Affichage des spectres
+figure;
+subplot(211);
+plot(fr, spx);
+grid on;
+axis([0 1000 0 1]);
+xlabel('Fréquence (Hz)');
+title('Spectre du signal x(t)');
+
+subplot(212);
+plot(fr, spy);
+grid on;
+axis([0 1000 0 1]);
+xlabel('Fréquence (Hz)');
+title('Spectre du signal y(t) après filtrage');
+
