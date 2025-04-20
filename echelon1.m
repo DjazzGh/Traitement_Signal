9477fec5e35d2b71189fa4dbe71156fdea45e33b
rachasalhi1 <162183624+rachasalhi1@users.noreply.github.com>
2025-04-20 19:24:10 +0200

    Update echelon1.m

--- echelon1.m
+++ echelon1.m
@@ -1,9 +1,6 @@
-% echelon1.m
-% Script pour tracer le signal y(t) = u(t - 1)
-
-t = -10:1/1000:10;         % Vecteur temps
-y = zeros(size(t));        % Initialisation du signal
-y(t >= 1) = 1;             % y(t) = 1 pour t >= 1
+t = -10:1/1000:10;        
+y = zeros(size(t));      
+y(t >= 1) = 1;           
 
 figure(4);
 plot(t, y, 'LineWidth', 2);
