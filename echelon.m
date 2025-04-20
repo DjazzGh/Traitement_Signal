71aa664a5b4584960af4a04c8c4102c16315b5d3
rachasalhi1 <162183624+rachasalhi1@users.noreply.github.com>
2025-04-20 19:24:58 +0200

    Update echelon.m

--- echelon.m
+++ echelon.m
@@ -1,13 +1,10 @@
-% Script : echelon.m
-% But : tracer le signal échelon unitaire u(t)
+t = -10:1/1000:10;     
+u = zeros(size(t));      
+u(find(t >= 0)) = 1;    
 
-t = -10:1/1000:10;        % Temps de -10 à 10 avec un pas de 0.001
-u = zeros(size(t));       % Initialisation de u(t) à zéro
-u(find(t >= 0)) = 1;      % u(t) = 1 pour t >= 0
-
-figure(3);                % Figure numéro 3
-plot(t, u, 'LineWidth', 2); % Tracé avec une ligne épaisse
-grid on;                  % Grille
+figure(3);               
+plot(t, u, 'LineWidth', 2); 
+grid on;                
 xlabel('temps en (s)');
 ylabel('amplitude en (v)');
 title('signal échelon unitaire u(t)');
