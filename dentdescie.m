732997926a3700f4f55375efa37536ace0b29eae
rachasalhi1 <162183624+rachasalhi1@users.noreply.github.com>
2025-04-20 19:25:55 +0200

    Update dentdescie.m

--- dentdescie.m
+++ dentdescie.m
@@ -1,14 +1,14 @@
 
-f = 1; % Fréquence du signal (en Hz)
-T = 1/f; % Période du signal
+f = 1; 
+T = 1/f; 
 t = 0:0.01:2*T; % Vecteur temps pour deux périodes (pas de 0.01s)
 
-% Génération du signal en dent de scie
+
 signal = sawtooth(2*pi*f*t); % Signal en dent de scie avec la fonction sawtooth
 
-% Visualisation
-figure(8); % Ouvre la figure 8
-plot(t, signal, 'r', 'LineWidth', 2); % Trace le signal
+
+figure(8); 
+plot(t, signal, 'r', 'LineWidth', 2); 
 title('Signal en Dent de Scie sur Deux Périodes');
 xlabel('Temps (s)');
 ylabel('Amplitude');
