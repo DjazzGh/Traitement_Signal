7aa13599e12d283cf08fc380f9381590d70bf230
rachasalhi1 <162183624+rachasalhi1@users.noreply.github.com>
2025-04-20 19:27:29 +0200

    Update delta1.m

--- delta1.m
+++ delta1.m
@@ -1,10 +1,10 @@
-t=-10:1/1000:10; %échelle du temps allant de -10 à +10 avec 1000
-%échantillons par seconde
-delta=zeros(size(t)); % remplissage par des zéros
+t=-10:1/1000:10; 
+
+delta=zeros(size(t));
 delta(find(t==0))=1; % delta =1 seulement pour t=0
-figure(1); % ouvrir une figure vide nommée figure1
+figure(1); 
 stem(t,delta); %tracege de delta
-grid; %grille
+grid; 
 xlabel('temps en (s)');
 ylabel('amplitude en (v)');
 title('signal impulsion de Dirac');
