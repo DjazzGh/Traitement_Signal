cce13a3a43a027d87441682ab6b3b1f1ed11ae85
rachasalhi1 <162183624+rachasalhi1@users.noreply.github.com>
2025-04-20 19:15:17 +0200

    Update sig_sin_axis.m

--- sig_sin_axis.m
+++ sig_sin_axis.m
@@ -1,14 +1,14 @@
 fe = 1000;           % Fréquence d'échantillonnage (Hz)
 f0 = 100;            % Fréquence du signal (Hz)
 t = -0.01:1/fe:0.01; % Domaine temporel pour 2 périodes (2 x 1/100 = 0.02 s)
-x = sin(2*pi*f0*t);  % Signal sinusoïdal
+x = sin(2*pi*f0*t);  
 
 figure(7);
-plot(t, x, 'b', 'LineWidth', 2); % Tracé du signal
+plot(t, x, 'b', 'LineWidth', 2); 
 grid on;
 xlabel('temps en (s)');
 ylabel('amplitude en (V)');
 title('Signal sinusoïdal sur deux périodes');
 
-% Limiter l'affichage à deux périodes
+% on Limite l'affichage à deux périodes
 axis([-0.01 0.01 -1.2 1.2]);
