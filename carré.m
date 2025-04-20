102ef047c711a28f756e57b8dbd94eafba21fb7e
rachasalhi1 <162183624+rachasalhi1@users.noreply.github.com>
2025-04-20 19:28:21 +0200

    Update carré.m

--- carré.m
+++ carré.m
@@ -1,14 +1,14 @@
 
-f = 1; % Fréquence du signal (en Hz)
-T = 1/f; % Période du signal
-t = 0:0.01:2*T; % Vecteur temps pour deux périodes (pas de 0.01s)
+f = 1; 
+T = 1/f; 
+t = 0:0.01:2*T; 
 
-% Génération du signal carré
-signal = square(2*pi*f*t); % Signal carré avec la fonction square
 
-% Visualisation
-figure(7); % Ouvre la figure 7
-plot(t, signal, 'b', 'LineWidth', 2); % Trace le signal
+signal = square(2*pi*f*t); 
+
+
+figure(7); 
+plot(t, signal, 'b', 'LineWidth', 2); 
 title('Signal Carré sur Deux Périodes');
 xlabel('Temps (s)');
 ylabel('Amplitude');
