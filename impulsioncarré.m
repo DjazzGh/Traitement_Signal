1dadee71ee1c177d8ea8c96da56b5221a6fb51d5
rachasalhi1 <162183624+rachasalhi1@users.noreply.github.com>
2025-04-20 19:18:35 +0200

    Update impulsioncarré.m

--- impulsioncarré.m
+++ impulsioncarré.m
@@ -1,7 +1,7 @@
-Fe = 1000; % Fréquence d'échantillonnage
-t = -10:1/Fe:10; % Échelle temps allant de -10 à +10 avec 1000 points entre chaque seconde
-p = zeros(size(t)); % Remplissage par des zéros
-id = find((t > -1) & (t <= 1)); % Détection des points de t allant de -1 à +1
+Fe = 1000; 
+t = -10:1/Fe:10;
+p = zeros(size(t)); 
+id = find((t > -1) & (t <= 1)); 
 p(id) = 1; % Construction d'une impulsion carrée de -1 à 1
 figure(1);
 plot(t, p, 'LineWidth', 2);
