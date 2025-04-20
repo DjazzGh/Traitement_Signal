ef63afdfc0f58aff1a48d1e0b317ef3e56bb3b9f
rachasalhi1 <162183624+rachasalhi1@users.noreply.github.com>
2025-04-20 19:13:46 +0200

    Update sinuscardinal.m

--- sinuscardinal.m
+++ sinuscardinal.m
@@ -1,12 +1,12 @@
-% Paramètres
+
 t = -2:0.01:2; % Vecteur temps pour deux "périodes" (de -2 à 2)
 
 % Génération du signal sinus cardinal
-signal = sinc(t); % Signal sinc avec la fonction sinc
+signal = sinc(t); 
 
-% Visualisation
+
 figure(10); % Ouvre la figure 10
-plot(t, signal, 'm', 'LineWidth', 2); % Trace le signal
+plot(t, signal, 'm', 'LineWidth', 2); 
 title('Signal Sinus Cardinal sur Deux "Périodes"');
 xlabel('Temps');
 ylabel('Amplitude');
