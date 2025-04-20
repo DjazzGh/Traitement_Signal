c3b3c08d21312fde10c2ac04c39ff234ea0b3527
rachasalhi1 <162183624+rachasalhi1@users.noreply.github.com>
2025-04-20 19:10:20 +0200

    Update triangulaire.m

--- triangulaire.m
+++ triangulaire.m
@@ -1,14 +1,14 @@
-% Paramètres
-f = 1; % Fréquence du signal (en Hz)
-T = 1/f; % Période du signal
-t = 0:0.01:2*T; % Vecteur temps pour deux périodes (pas de 0.01s)
-width = 0.5; % Paramètre WIDTH pour un signal triangulaire (entre 0 et 1)
 
-% Génération du signal triangulaire
-signal = sawtooth(2*pi*f*t, width); % Signal triangulaire avec la fonction sawtooth
+f = 1; % en Hz
+T = 1/f; 
+t = 0:0.01:2*T; 
+width = 0.5;
 
-% Visualisation
-figure(9); % Ouvre la figure 9
+% Génération du signal triangulaire avec la fonction sawtooth
+signal = sawtooth(2*pi*f*t, width); 
+
+
+figure(9); 
 plot(t, signal, 'g', 'LineWidth', 2); % Trace le signal
 title('Signal Triangulaire sur Deux Périodes');
 xlabel('Temps (s)');
