abf0d317a7961a74c3fc8a00fd46e00e6cb5b4bb
rachasalhi1 <162183624+rachasalhi1@users.noreply.github.com>
2025-04-20 19:23:32 +0200

    Update filtragex.m

--- filtragex.m
+++ filtragex.m
@@ -1,6 +1,3 @@
-% ===== Question 3 - Application du filtre =====
-
-% Données du filtre
 fa = 200;           % fréquence de la bande passante
 fc = 350;           % fréquence de la bande atténuée
 wa = 2*pi*fa;       % pulsation de fa
@@ -16,13 +13,13 @@ rc = 40;            % Atténuation min en bande coupée (dB)
 fe = 2000;           % fréquence d’échantillonnage
 f1 = 100;            % fréquence dans la bande passante
 f2 = 400;            % fréquence hors bande
-t = 0:1/fe:1;        % vecteur temps de 1 seconde
+t = 0:1/fe:1;       
 
 x1 = cos(2*pi*f1*t); % composante à 100 Hz
 x2 = cos(2*pi*f2*t); % composante à 400 Hz
 x = x1 + x2;         % signal composé
 
-% Application du filtre analogique au signal
+
 sys = tf(b, a);        % création de la fonction de transfert
 y = lsim(sys, x, t);   % sortie filtrée du signal
 
